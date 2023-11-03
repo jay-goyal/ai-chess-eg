7685fba772b947588a77e95a348e2611cc22ee6c
Jay Goyal <goyal.jay2003@gmail.com>
2023-11-03 17:18:45 +0000

    Initial setup

--- main.py
+++ main.py
@@ -1,8 +1,45 @@
 import os
 from stockfish import Stockfish
 from dotenv import load_dotenv
+import chess
+import random
+import util
 
 
 load_dotenv()
 sfPath = os.environ.get("SF_PATH", "stockfish\\stockfish-windows-x86-64-avx2.exe")
-sf = Stockfish(sfPath)
+stockfish = Stockfish(sfPath)
+
+# Initialize Board
+K, Q, k = -1, -1, -1
+while not util.fenCheck(K, Q, k):
+    K, Q, k = random.sample(range(0, 63), 3)
+
+fen = util.generateFen(K, Q, k)
+stockfish.set_fen_position(fen)
+board = chess.Board(fen)
+
+os.system("cls" if os.name == "nt" else "clear")
+print(stockfish.get_best_move())
+print(stockfish.get_evaluation())
+print(fen)
+print(stockfish.get_board_visual())
+
+# Play the game
+while not util.checkMate(stockfish):
+    print("Press any key for next position")
+    input()
+    os.system("cls" if os.name == "nt" else "clear")
+    evaluation = stockfish.get_evaluation()
+    stockfish.make_moves_from_current_position([stockfish.get_best_move()])
+    board = chess.Board(stockfish.get_fen_position())
+    if util.checkMate(stockfish):
+        print(
+            "CHECK MATE. ",
+            util.whoPlayedLast(stockfish.get_fen_position()),
+            " WINS!",
+        )
+    print(stockfish.get_fen_position())
+    print(stockfish.get_board_visual())
+
+print("GAME OVER")
---
+++ util.py
@@ -0,0 +1,82 @@
+def generateFen(K, Q, k):
+    board_pos = [["." for _ in range(8)] for _ in range(8)]
+    board_pos[K // 8][K % 8] = "K"
+    board_pos[Q // 8][Q % 8] = "Q"
+    board_pos[k // 8][k % 8] = "k"
+    ans = ""
+    for row in board_pos:
+        count_empty = 0
+        for cell in row:
+            if cell == ".":
+                count_empty += 1
+            else:
+                if count_empty > 0:
+                    ans += str(count_empty)
+                    count_empty = 0
+                ans += cell
+        if count_empty > 0:
+            ans += str(count_empty)
+        ans += "/"
+
+    ans = ans[:-1]  # Remove the last '/'
+    ans += " w - - 0 1"
+    return ans
+
+
+def inKingRange(K, k):
+    if abs(K // 8 - k // 8) <= 1 and abs(K % 8 - k % 8) <= 1:
+        return True
+    return False
+
+
+def inQueenRange(Q, k):
+    # TODO: Better Checks
+    if Q // 8 == k // 8 or Q % 8 == k % 8:
+        return True
+    if Q // 8 - Q % 8 == k // 8 - k % 8 or Q // 8 + Q % 8 == k // 8 + k % 8:
+        return True
+    return False
+
+
+def fenCheck(K, Q, k):
+    if inKingRange(K, k) or inQueenRange(Q, k):
+        return False
+    return True
+
+
+def getMatRepFen(fen):
+    matrix = [["."] * 8 for _ in range(8)]
+    inp = fen.split()[0]
+    row = 0
+    col = 0
+
+    for char in inp:
+        if char.isdigit():
+            col += int(char)
+        elif char == "/":
+            row += 1
+            col = 0
+        else:
+            matrix[row][col] = char
+            col += 1
+
+    return matrix
+
+
+def whoPlayedLast(fen):
+    x = fen.split()[1]
+    if x == "w":
+        return "BLACK"
+    return "WHITE"
+
+
+def checkMate(stockfish):
+    evaluation = stockfish.get_evaluation()
+    if evaluation["type"] == "mate" and evaluation["value"] == 0:
+        return True
+    return False
+
+
+def checkStalemate(stockfish):
+    # TODO: Implement method
+    pass
